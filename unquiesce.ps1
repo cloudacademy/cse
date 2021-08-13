a2528b60284488621fc024b489f6119718740983
adilislamca <85554623+adilislamca@users.noreply.github.com>
2021-08-13 17:19:29 +0100

    Update unquiesce.ps1

--- unquiesce.ps1
+++ unquiesce.ps1
@@ -1,1 +1,1 @@
-"& 'C:\Program Files\Azure DevOps Server 2019\Tools\TFSServiceControl.exe' unquiesce"
+powershell.exe -command "& 'c:\Program Files\Azure DevOps Server 2019\Tools\TFSServiceControl.exe' unquiesce"
